7d497d4b89b616fdcaf601f72eb71b7a2bf3d4ad
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-02-28 15:08:56 +0530

    update Vite configuration to set base path to root

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000, open: true, host: '0.0.0.0' },
-  base: '/MRKUNTALOJHA/',
+  base: '/',
 });
