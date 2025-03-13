54ff3810b300b37194b0078ac95ffad8dc0d6521
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-03-13 15:18:52 +0530

    comment out server configuration in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, open: true, host: '0.0.0.0' },
+  // server: { port: 3000, open: true, host: '0.0.0.0' },
   base: '/',
 });
